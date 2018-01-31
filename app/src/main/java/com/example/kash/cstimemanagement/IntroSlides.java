e5140cbde9b5da1c5591da578d1f6758c55f123f
Kash <kashpitan@googlemail.com>
2018-01-31 23:22:27 +0000

    slider mostly complete database half complete

--- app/src/main/java/com/example/kash/cstimemanagement/IntroSlides.java
+++ app/src/main/java/com/example/kash/cstimemanagement/IntroSlides.java
@@ -3,14 +3,22 @@ package com.example.kash.cstimemanagement;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class IntroSlides extends AppCompatActivity {
 
     private ViewPager mSlideViewPager;
     private LinearLayout mDotLayout;
 
+    private TextView[] mDots;
+
     private SliderAdapter sliderAdapter;
+
+    private Button NextButton;
+    private Button BackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +32,47 @@ public class IntroSlides extends AppCompatActivity {
 
         mSlideViewPager.setAdapter(sliderAdapter);
 
+        NextButton = (Button) findViewById(R.id.buttonNext);
+        BackButton = (Button) findViewById(R.id.buttonBack);
+
+        dots(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
     }
+
+    public void dots(int position){
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for(int i = 0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226"));
+            mDots[i].setTextColor(getResources().getColor(R.color.dotInactive));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.dotActive));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            dots(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
